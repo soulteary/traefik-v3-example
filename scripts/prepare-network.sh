9988fedad2df8a11cacaa1fe6c08618a8938ff1d
Su Yang <soulteary@gmail.com>
2024-08-04 18:19:29 +0800

    chore: update scripts

--- scripts/prepare-network.sh
+++ scripts/prepare-network.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 NETWORK_NAME=traefik
 
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then
-    docker network create ${NETWORK_NAME} ; 
+    # 创建 Docker 网络
+    output=$(docker network create ${NETWORK_NAME});
+    # 检查输出是否为64位长度的字符串
+    if [[ ${#output} -eq 64 ]]; then
+        echo "create docker network for traefik ok"
+    else
+        echo "Network creation failed or output is not 64 characters long."
+    fi
 fi
